40da7afb630fd841bb587fa894cfcdf377a3cf13
Ghaithgaied98 <ghaithgaied10@gmail.com>
2021-02-25 09:36:53 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {Form,Button} from 'react-bootstrap'
+import Col from 'react-bootstrap/Col'
+import Media from 'react-bootstrap/Media'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
+
+function App() {
+  return (
+    <div class = "App">
+    <h1>Welcome to my checkpoint</h1>
+
+    <Container>
+  <Row>
+    <Col>1 of 1</Col>
+  </Row>
+</Container>
+    <Form>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
+
+  <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    </div>
+  );
+}
+
+export default App;
